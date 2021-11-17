160a7d4bdb957ca92d657531387a53fad5f298b5
IAS\Ian.Nkwocha <ian.nkwocha@iris.co.uk>
2021-11-17 15:38:48 +0000

    Completed email extraction exercise

--- C#/EmailExtraction/EmailExtraction/Program.cs
+++ C#/EmailExtraction/EmailExtraction/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace EmailExtraction
 {
@@ -6,7 +9,36 @@ namespace EmailExtraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                string text = File.ReadAllText("C:\\Users\\Ian.Nkwocha\\OneDrive\\Documents\\Development\\Training\\Bootcamp\\C#\\EmailExtraction\\EmailExtraction\\sample.txt");
+                Dictionary<string, int> emails = new();
+
+
+                Regex emailRegex = new(@"([\w\.\-])+(\@)+[A-z]+(\.)+(\w){2,3}\.*(\w)*");
+                MatchCollection matches = emailRegex.Matches(text);
+                Console.WriteLine("{0} emails found", matches.Count);
+
+                foreach(Match match in matches)
+                {
+                    string domain = match.ToString()[match.ToString().IndexOf("@")..].Trim();
+                    if(emails.TryAdd(domain, 1)){
+                        Console.WriteLine(domain);
+                    }
+                    else
+                    {
+                        emails[domain]++;
+                    }
+
+                }
+                Console.WriteLine("{0} softwire emails found", emails["@softwire.com"]);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
